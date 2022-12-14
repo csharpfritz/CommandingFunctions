79b0b6e16ab22a4faf7c982e8b3a2d8e71aad756
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-12-14 08:48:24 -0500

    First commit

---
+++ Fritz.CommandingFunctions/FunctionsGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Fritz.CommandingFunctions;
+
+public class FunctionsGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+      context.RegisterForSyntaxNotifications(() => new CommandSyntaxReceiver());
+    }
+
+}
+
+public class CommandSyntaxReceiver : ISyntaxReceiver
+{
+  public List<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
+
+  public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+  {
+    // any class with at least one attribute is a candidate for property injection
+    if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax &&
+        classDeclarationSyntax.AttributeLists.Count > 0)
+    {
+      CandidateClasses.Add(classDeclarationSyntax);
+    }
+  }
+}
---
+++ Fritz.CommandingFunctions/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Fritz.CommandingFunctions;
+
+public interface ICommand
+{
+
+  ILogger Logger { get; set; }
+
+  /// <summary>
+  /// Execute the command asyncronously
+  /// </summary>
+  /// <returns></returns>
+  Task ExecuteAsync();
+
+  void RegisterTriggers(ICommandConfiguration config);
+
+}
